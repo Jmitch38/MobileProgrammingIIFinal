aad89767a8402e6b418708a214e3bc2fd8b5bec1
Jmitch38 <14fansemail@gmail.com>
2018-04-04 17:08:43 -0400

    wORKING VERSION OF uNITY FOR PUBLISHING GAME

---
+++ MobileProgrammingIIFinal/Assets/Scripts/CameraController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    float Speed = 0.1f;
+
+	void Update ()
+    {
+        if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Moved)
+        {
+            // Get movement of the finger since last frame
+            Vector3 touchDeltaPosition = Input.GetTouch(0).deltaPosition;
+
+            // Move object across Z plane
+            transform.Translate(-touchDeltaPosition.x * 0, -touchDeltaPosition.y * 0, -touchDeltaPosition.z * Speed);
+        }
+    }
+}
