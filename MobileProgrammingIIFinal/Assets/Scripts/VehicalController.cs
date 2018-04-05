501ed45ddbeedcde68ffd7e46488b9897ffa6c48
Jmitch38 <14fansemail@gmail.com>
2018-04-05 11:55:16 -0400

    Working on Ai movement

--- MobileProgrammingIIFinal/Assets/Scripts/VehicalController.cs
+++ MobileProgrammingIIFinal/Assets/Scripts/VehicalController.cs
@@ -5,19 +5,19 @@ using UnityEngine;
 public class VehicalController : MonoBehaviour
 {
     int CurrentWayPoint;
-
+    Vector3 Temp;
     public float speed;
     public float turnspeed;
 
     void Start()
     {
-        Transform[] Road = GameObject.Find("Manager's").GetComponent<WayPoint>().WayPoints;
         CurrentWayPoint = 0;
     }
 
     void Update()
     {
-        if (gameObject.transform.position != Road[CurrentWayPoint])
+        Vector3 Temp = WayPoint.WP.Waypoint[CurrentWayPoint].transform.position;
+        if (this.gameObject.transform.position != Temp)
         {
             MoveTowards();
         }
@@ -25,6 +25,17 @@ public class VehicalController : MonoBehaviour
 
     void MoveTowards()
     {
+        transform.position += Vector3.forward * -speed * Time.deltaTime;
+        Vector3.RotateTowards(transform.forward, Temp, 360,turnspeed);
+    }
 
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Waypoint")
+        {
+            CurrentWayPoint += 1;
+            print(CurrentWayPoint);
+            print(Temp);
+        }
     }
 }
--- MobileProgrammingIIFinal/Assets/Scripts/WayPoint.cs
+++ MobileProgrammingIIFinal/Assets/Scripts/WayPoint.cs
@@ -4,5 +4,19 @@ using UnityEngine;
 
 public class WayPoint : MonoBehaviour
 {
-    public Transform[] WayPoints;
+    public static WayPoint WP;
+    public GameObject[] Waypoint;
+
+    private void Awake()
+    {
+        if (!WP) WP = this;
+        else Destroy(this);
+    }
+
+
+    private void Start()
+    {
+        Waypoint = GameObject.FindGameObjectsWithTag("Waypoint");
+    }
+    
 }
