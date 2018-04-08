732bf29a96d9183a849f8760c7a0b6b778e9dd55
Jmitch38 <14fansemail@gmail.com>
2018-04-08 15:44:19 -0400

    NavMesh

---
+++ MobileProgrammingIIFinal/Assets/Scripts/MoveToAgent.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class MoveToAgent : MonoBehaviour
+{
+    public Transform Gate;
+
+	void Start ()
+    {
+        NavMeshAgent agent = GetComponent<NavMeshAgent>();
+        agent.destination = Gate.position;
+    }
+
+}
