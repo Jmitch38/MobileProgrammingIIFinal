98d75659888d8928894b3c040d0a9fa7a9937418
Jmitch38 <14fansemail@gmail.com>
2018-04-15 19:50:11 -0400

    redid road tiles, brought in new tower head for Light TOwer

--- MobileProgrammingIIFinal/Assets/Scripts/AttackingAI/Fire.cs
+++ MobileProgrammingIIFinal/Assets/Scripts/AttackingAI/Fire.cs
@@ -10,7 +10,7 @@ public class Fire : MonoBehaviour
     public GameObject Laser;
     float speed = 1f;
 
-    //Firing
+    //where lasers come from
     public Transform[] FiringSpots;  
 
     void Update()
@@ -20,16 +20,11 @@ public class Fire : MonoBehaviour
         KeepFiringAssholes();
     }
 
-    void LookAtEnemy()
+    void LookAtEnemy() //gun mount turns towards enemy
     {
         Vector3 targetDir = target.position - transform.position;
-
-        // The step size is equal to speed times frame time.
         float step = speed * Time.deltaTime;
-
         Vector3 newDir = Vector3.RotateTowards(transform.forward, targetDir, step, 0.0f);
-        Debug.DrawRay(transform.position, newDir, Color.red);
-
         // Move our position a step closer to the target.
         transform.rotation = Quaternion.LookRotation(newDir);
     }
