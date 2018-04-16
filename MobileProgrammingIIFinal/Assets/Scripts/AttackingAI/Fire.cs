f4f08e02e519c2f831335e68dd5d6c3b498ecc46
Jmitch38 <14fansemail@gmail.com>
2018-04-16 19:56:41 -0400

    Laser spawning now working

--- MobileProgrammingIIFinal/Assets/Scripts/AttackingAI/Fire.cs
+++ MobileProgrammingIIFinal/Assets/Scripts/AttackingAI/Fire.cs
@@ -6,23 +6,25 @@ public class Fire : MonoBehaviour
 {
     //Targeting
     public Transform[] Gates;
-    public Transform target;  
-    float speed = 15f;
+    public Transform Currenttarget;
+    public float speed;
 
     //where lasers come from
     public Transform[] FiringSpots;
     public GameObject Laser;
+    public float ReloadTimer;
 
     void Update()
     {
         LookAtEnemy();
         WhichGateisAlive();
         KeepFiringAssholes();
+        ResetReloadTimer();
     }
 
     void LookAtEnemy() //gun mount turns towards enemy
     {
-        Vector3 direction = (target.transform.position - transform.position).normalized;
+        Vector3 direction = (Currenttarget.transform.position - transform.position).normalized;
         Quaternion lookRot = Quaternion.LookRotation(new Vector3(direction.x, 0, direction.z));
         transform.rotation = Quaternion.Slerp(transform.rotation, lookRot, Time.deltaTime * speed);
     }
@@ -34,6 +36,24 @@ public class Fire : MonoBehaviour
 
     void KeepFiringAssholes()
     {
+        float Distance = Vector3.Distance(Currenttarget.position, transform.position);
+        if(Distance <= 5 && ReloadTimer <= 0)
+        {
+            Instantiate(Laser, FiringSpots[0].transform.position, FiringSpots[0].transform.rotation);
+            Instantiate(Laser, FiringSpots[1].transform.position, FiringSpots[1].transform.rotation);
+            ReloadTimer += 5;
+        }
+    }
 
+    void ResetReloadTimer()
+    {
+        if(ReloadTimer > 0)
+        {
+            ReloadTimer -= 1 * Time.deltaTime;
+        }
+        else
+        {
+            ReloadTimer = 0;
+        }
     }
 }
