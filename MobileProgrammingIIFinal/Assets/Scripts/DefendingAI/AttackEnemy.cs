7f84f61bf787e988bfbf667dbf8b76d213cea39b
Jmitch38 <14fansemail@gmail.com>
2018-04-24 19:22:26 -0400

    Turrets now target enemies

--- MobileProgrammingIIFinal/Assets/Scripts/DefendingAI/AttackEnemy.cs
+++ MobileProgrammingIIFinal/Assets/Scripts/DefendingAI/AttackEnemy.cs
@@ -4,28 +4,49 @@ using UnityEngine;
 
 public class AttackEnemy : MonoBehaviour
 {
-    Transform CurrentTarget;
+    //under any circumstance DO NOT TOUCH THIS CODE
+    #region
+    private List<Transform> targets = new List<Transform>();
+    //Transform CurrentTarget;
+    private Transform _primaryTarget;
+    //finds an important target to focus on and caches the reference
+    public Transform Target
+    {
+        get
+        {
+            if (_primaryTarget == null)
+            {
+                // clean the targets list by removing each target transform
+                // that is nulled due to the gameobject already being destroyed
+                targets.RemoveAll(eachTarget => { return eachTarget == null; });
 
+                if (targets.Count > 0)
+                {
+                    // find the first element that would cause the lambada to return true
+                    // you can change this later to allow the tower to prioritize different targets
+                    _primaryTarget = targets.Find(target => { return true; });
+                }
+            }
+            return _primaryTarget;
+        }
+    }
+    #endregion 
+
+    [Header("--Turret Stats--")]
     public int Health;
     public float TurnSpeed;
     public float ReloadTimer;
 
-	void Update ()
+    void Update()
     {
         LookAtEnemy();
-        TargetEnemy();
-	}
+    }
 
     void LookAtEnemy()
     {
-        Vector3 direction = (CurrentTarget.transform.position - transform.position).normalized;
+        Vector3 direction = (Target.transform.position - transform.position).normalized;
         Quaternion lookRot = Quaternion.LookRotation(new Vector3(direction.x, 0, direction.z));
         transform.rotation = Quaternion.Slerp(transform.rotation, lookRot, Time.deltaTime * TurnSpeed);
-    }
-
-    void TargetEnemy()
-    {
-
     }
 
     void FixedUpdate()
@@ -38,5 +59,24 @@ public class AttackEnemy : MonoBehaviour
         {
             ReloadTimer = 0;
         }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Vechical"))
+        {
+            targets.Add(other.transform);
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        //in case it was the primary target that leaves the Tower's range
+        if (ReferenceEquals(_primaryTarget, other.transform))
+        {
+            _primaryTarget = null;
+        }
+
+        targets.Remove(other.transform);
     }
 }
