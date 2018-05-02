e5051eca24a3152a0737785628a7adf1aeaab407
Jmitch38 <14fansemail@gmail.com>
2018-05-02 19:35:03 -0400

    CameraController now working

---
+++ MobileProgrammingIIFinal/Assets/Scripts/CameraScripts/CameraController.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CameraController : MonoBehaviour
+{
+    public static float CamSpeedUpandDown;
+    public static float CamSpeedLeftandRight;
+
+    void Update()
+    {
+        transform.Translate(Vector3.forward * CamSpeedLeftandRight * Time.deltaTime);
+        transform.Translate(Vector3.right * CamSpeedUpandDown * Time.deltaTime);
+    }
+}
